d93ea1802f94a89452e7583ce771b36efd313571
tim@trakeo.com <tim@trakeo.com>
2017-02-14 17:07:50 +0000

    deleted plugin `Logo Showcase` version 1.6

--- wp-content/plugins/logo-showcase/admin/js/color-picker.js
+++
@@ -1,4 +0,0 @@
-jQuery(document).ready(function(jQuery){
-    jQuery('#sortable').sortable();
-    jQuery('#logo-showcase-column-border-color, #accordions-table-field-bg-font-color,  #accordions-table-field-content-font-color, #accordions-table-field-content-bg-color, #logo-showcase-column-border-hover-color, #logo-showcase-column-title-font-color').wpColorPicker();        
-});
