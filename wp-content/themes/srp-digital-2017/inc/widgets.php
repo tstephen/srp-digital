84c5f0d1568b30c666d8ea1f9f5312dc64fe767d
tim@trakeo.com <tim@trakeo.com>
2017-02-14 18:28:29 +0000

    modified theme `SRP Digital 2017` version 1.0.0

--- wp-content/themes/srp-digital-2017/inc/widgets.php
+++ wp-content/themes/srp-digital-2017/inc/widgets.php
@@ -15,31 +15,5 @@ function srp_widgets_init() {
     'after_title'     => '</h4>',
   ) );
 
-  /*
-   * Principles (one full width widget area)
-   */
-  register_sidebar( array(
-    'name'            => __( 'Principles', 'srp-digital-2017' ),
-    'id'              => 'principles-widget-area',
-    'description'     => __( 'The principles widget area', 'srp-digital-2017' ),
-    'before_widget'   => '<div class="%1$s %2$s col-sm-12">',
-    'after_widget'    => '</div>',
-    'before_title'    => '<h4>',
-    'after_title'     => '</h4>',
-  ) );
-
-  /*
-   * Products Slider (one full width widget area)
-   */
-  register_sidebar( array(
-    'name'            => __( 'Products Slider ', 'srp-digital-2017' ),
-    'id'              => 'products-slider-widget-area',
-    'description'     => __( 'The products slider widget area', 'srp-digital-2017' ),
-    'before_widget'   => '<div class="%1$s %2$s product col-sm-12">',
-    'after_widget'    => '</div>',
-    'before_title'    => '<h4>',
-    'after_title'     => '</h4>',
-  ) );
-
 }
 add_action( 'widgets_init', 'srp_widgets_init' );
