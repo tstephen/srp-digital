1b48c850fca5ceebf0d239fbcb93b6348180a6f3
Tim Stephenson <tim@trakeo.com>
2018-01-04 11:59:56 +0000

    resolves #603

--- srp/2.0.0/js/base.js
+++ srp/2.0.0/js/base.js
@@ -417,7 +417,8 @@ var BaseRactive = Ractive.extend({
       return link.substring(0,idx);
     }
   },
-  switchToTenant: function(tenant) {
+  switchToTenant: function() {
+    var tenant = $('select.tenant').val();
     if (tenant==undefined || typeof tenant != 'string') {
       return false;
     }
